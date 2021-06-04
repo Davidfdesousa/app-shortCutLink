7c84ba438ce55b1530e0686da48e9660d87f20f8
David Ferrira <david.sousa@globalhitss.com.br>
2021-06-04 18:35:39 -0300

    estilizando drawer navigation

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,51 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import Home from "./pages/Home";
 import MyLinks from "./pages/MyLinks";
 
+import { Ionicons } from "@expo/vector-icons";
+
 const Drawer = createDrawerNavigator();
 
 function Routes() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={Home} />
+    <Drawer.Navigator
+      drawerContentOptions={{
+        activeBackgroundColor: "#2ccbb9",
+        activeTintColor: "#fff",
+        marginTop: 16,
+        labelStyle: {
+          fontSize: 18,
+        },
+      }}
+    >
+      <Drawer.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: "Encurtar Link",
+          drawerIcon: ({ focused, size, color }) => (
+            <Ionicons
+              name={focused ? "cube" : "cube-outline"}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
 
-      <Drawer.Screen name="MyLinks" component={MyLinks} />
+      <Drawer.Screen
+        name="MyLinks"
+        component={MyLinks}
+        options={{
+          title: "Meus Links",
+          drawerIcon: ({ focused, size, color }) => (
+            <Ionicons
+              name={focused ? "stats-chart" : "stats-chart-outline"}
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 }
