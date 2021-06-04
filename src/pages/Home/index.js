20f2bdb23cc5db9b28bd6f4d0f5f288536d17f8b
David Ferrira <david.sousa@globalhitss.com.br>
2021-06-04 19:31:09 -0300

    estilização do statusbar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text } from "react-native";
 
 import { LinearGradient } from "expo-linear-gradient";
+import StatusBarPage from '../../components/StatusBarPage'
 
 export default function Home() {
   return (
@@ -9,6 +10,10 @@ export default function Home() {
       colors={["#1ddbb9", "#132742"]}
       style={{ flex: 1, justifyContent: "center" }}
     >
+      <StatusBarPage
+        barStyle="light-content"
+        backgroundColor="#1ddbb9"
+      />
       <Text>Pagina Home </Text>
     </LinearGradient>
   );
--- src/pages/MyLinks/index.js
+++ src/pages/MyLinks/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import StatusBarPage from "../../components/StatusBarPage";
+
 export default function MyLinks() {
   return (
     <View>
+      <StatusBarPage barStyle="light-content" backgroundColor="#132742" />
       <Text>Pagina MyLinks </Text>
     </View>
   );
