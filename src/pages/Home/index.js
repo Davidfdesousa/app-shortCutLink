8e9b3649b2c257960be5a9e65616e49566822624
David Ferrira <david.sousa@globalhitss.com.br>
2021-06-06 19:36:58 -0300

    inclusao do componente menu, logo, input e button na pagina home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,22 @@ import React from "react";
 import { View, Text } from "react-native";
 
 import { LinearGradient } from "expo-linear-gradient";
-import StatusBarPage from '../../components/StatusBarPage'
+import StatusBarPage from "../../components/StatusBarPage";
+import Menu from "../../components/Menu";
+import {
+  BoxIcon,
+  ButtonLink,
+  ButtonLinkText,
+  ContainerContent,
+  ContainerInput,
+  ContaninerLogo,
+  Input,
+  Logo,
+  Subtitle,
+  Title,
+} from "./styles";
+
+import { Feather } from "@expo/vector-icons";
 
 export default function Home() {
   return (
@@ -10,11 +25,35 @@ export default function Home() {
       colors={["#1ddbb9", "#132742"]}
       style={{ flex: 1, justifyContent: "center" }}
     >
-      <StatusBarPage
-        barStyle="light-content"
-        backgroundColor="#1ddbb9"
-      />
-      <Text>Pagina Home </Text>
+      <StatusBarPage barStyle="light-content" backgroundColor="#1ddbb9" />
+
+      <Menu />
+
+      <ContaninerLogo>
+        <Logo
+          source={require("../../assets/logo.png")}
+          resizeMode={"contain"}
+        />
+      </ContaninerLogo>
+
+      <ContainerContent>
+        <Title>Link</Title>
+        <Subtitle>Cole seu link para encurtar</Subtitle>
+
+        <ContainerInput>
+          <BoxIcon>
+            <Feather name={"link"} color="#fff" size={22} />
+          </BoxIcon>
+          <Input
+            placeholder="Cole seu link aqui ..."
+            placeholderTextColor="#fff"
+          />
+        </ContainerInput>
+
+        <ButtonLink>
+          <ButtonLinkText>Gerar Link </ButtonLinkText>
+        </ButtonLink>
+      </ContainerContent>
     </LinearGradient>
   );
 }
